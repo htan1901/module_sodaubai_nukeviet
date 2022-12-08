c6b1d31650893a7ea9a7eadeb79d33095d40815c
Xuan Nhan <nguyenxuannhan407@gmail.com>
2022-12-08 07:57:27 +0700

    Update LICENSE GPLv3.0

--- modules/schoolbook/language/admin_vi.php
+++ modules/schoolbook/language/admin_vi.php
@@ -4,7 +4,7 @@
  * @Project NUKEVIET 4.x
  * @Author 4FT 
  * @Copyright (C) 2022 4FT. All rights reserved
- * @License GNU/GPL version 2 or any later version
+ * @License GNU/GPL version 3 or any later version
  * @Createdate Wed, 12 Dec 2022 07:00:00 GMT+7
  */
 
